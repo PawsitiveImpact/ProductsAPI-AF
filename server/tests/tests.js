56e17b83e89cbff1a6ccb1b98077c8b75c71df55
Aidan Fisher <121323175+aidanFisher97@users.noreply.github.com>
2023-05-01 19:12:05 -0700

    Update tests.js

--- server/tests/tests.js
+++ server/tests/tests.js
@@ -16,14 +16,7 @@ describe('each request should return a status code of 200', () => {
                 expect(result).to.have.status(200);
               })
   })
-  // it('get request for a product style should have status code of 200', () => {
-  //   return chakram
-  //             .get('http://localhost:3000/products/1/styles')
-  //             .timeout(5000)
-  //             .then((result) => {
-  //               expect(result).to.have.status(200);
-  //             })
-  // })
+
   it('get request for a related item should have status code of 200', () => {
     return chakram
               .get('http://localhost:3000/products/1/related')
